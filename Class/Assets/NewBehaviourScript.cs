bc21a7ff8f22762ee963c35537c48b04d7d3f711
2018130 <35947698+2018130@users.noreply.github.com>
2022-07-12 19:47:35 +0900

    update22.07.12

---
+++ Class/Assets/NewBehaviourScript.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    int health;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = 100;
+        Debug.Log(health);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
